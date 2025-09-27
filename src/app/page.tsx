b30474ea7bb5b509551787210ba0f8bb4f2dd188
Dawood Ahmed <gfxwork66@gmail.com>
2025-09-27 17:56:54 +0500

    feat: add loading between logging in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import Loader from "@/components/Loader/page";
 
 export default function Home() {
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const loggedIn = sessionStorage.getItem("loggedIn");
@@ -13,7 +15,19 @@ export default function Home() {
     } else {
       router.push("/Login");
     }
+
+    const timer = setTimeout(() => setLoading(false), 5000);
+    return () => clearTimeout(timer);
   }, [router]);
+
+  if (loading) {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <Loader />
+        <p className="mt-4 text-gray-600 font-medium">Redirecting...</p>
+      </div>
+    );
+  }
 
   return null;
 }
